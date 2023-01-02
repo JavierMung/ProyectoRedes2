83b6f7e6ffb524d20356bc90b8c5afec8ac4a0db
JavierMung <javiermunguia35@gmail.com>
2023-01-01 21:56:28 -0600

    Subiendo todo el proyecto

--- src/App.js
+++ src/App.js
@@ -1,23 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import "./style.css"
+import { HiArrowCircleDown } from "react-icons/hi";
+import { useEffect, useRef, useState } from "react";
+import { motion } from "framer-motion";
 
 function App() {
+  const [files, setFiles] = useState([])
+  const file_target = useRef(null)
+  const [isOn, setIsOn] = useState(false);
+
+  const toggleSwitch = () => {
+    setIsOn(!isOn)
+    //background-image:url("../public/wave.svg"); 
+    document.body.setAttribute("data-ison",isOn)
+    
+  }
+  const spring = {
+    type: "spring",
+    stiffness: 700,
+    damping: 30
+  };
+ 
+  useEffect(() => {
+    fetch('http://localhost:8080/getdata')
+      .then(res => res.json())
+      .then(res => setFiles(res))
+      .catch((err) => console.error(err))
+
+  }, []);
+  const saveFile = () => {
+    if (file_target.current.value) {
+      fetch('http://localhost:8080/uploadfile', {//?nombre=Archivo56&fecha=10/10/10
+        method: "POST",
+        body: JSON.stringify({ nombre: file_target.current.value, fecha: new Date(Date.now()).toLocaleDateString() }),
+        headers: { "content-type": "application/json; chsrset=UTF-8" }
+      })
+        .then(res => res.json())
+        .then(res => setFiles(res))
+        .catch((err) => console.error(err))
+    }
+  }
+
+  const downloadButton = (event, name) => {
+    console.log(name)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="body" data-ison={isOn}>
+      <div className="box">
+        <div className={"switch"} data-ison={isOn} onClick={toggleSwitch}>
+          <motion.div whileHover={{ scale: 1.2 }} className={"handle"} layout transition={spring} />
+        </div>
+      </div>
+      <header className="title">
+        <h1>SAVE YOUR FILES</h1>
       </header>
+      <div>
+        <section className="buttons">
+          <div className="button_box">
+            <div className="input-group mb-3">
+              <input ref={file_target} type="file" className="form-control" id="inputGroupFile01" />
+              <button type="button" className="btn btn-primary" onClick={saveFile}>Save</button>
+            </div>
+          </div>
+        </section>
+        <section  className="tabla ">
+          <table  className="table text-light">
+            <thead className="tabla2" data-ison={isOn}>
+              <tr>
+                <th scope="col">Name</th>
+                <th scope="col">Upload Date</th>
+                <th scope="col">Download</th>
+              </tr>
+            </thead>
+            <tbody>
+              {files.length > 0 ? (files.map((file, index) => {
+                let name = file.split(" ")[0]
+                let date = file.split(" ")[1]
+                return (
+                  <tr className="tabla2" data-ison={isOn} key={index} >
+                    <th className="fs-6 fw-normal" scope="col">{name}</th>
+                    <th scope="col">{date}</th>
+                    <td><button className={"downloadButton"} onClick={(event) => downloadButton(event, name)}><HiArrowCircleDown color={"#05be3c"} size={30} /></button></td>
+                  </tr>
+                )
+              })) : (<></>)}
+
+            </tbody>
+          </table>
+        </section>
+      </div>
     </div>
   );
 }
